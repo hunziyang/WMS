68b8b637f80c6368abf9f00dd59723a4898e54bc
Tommy Wang <814230847@qq.com>
2023-02-15 21:33:51 +0800

    base:init

---
+++ src/main/java/com/yang/wms/WMSAppliction.java
@@ -0,0 +1,12 @@
+package com.yang.wms;
+
+import com.yang.portal.core.annotation.YangApplication;
+import com.yang.portal.core.utils.SpringUtil;
+
+@YangApplication
+public class WMSAppliction {
+
+    public static void main(String[] args) {
+        SpringUtil.run(WMSAppliction.class, args);
+    }
+}
